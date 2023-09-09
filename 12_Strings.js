c30ef6d51ef93bfc511d0e14bd063c671d32e0ef
M.Raahim Rizwan <raahimrizwantcs@gmail.com>
2023-09-09 14:01:19 +0500

    Strings in JS

---
+++ 12_Strings.js
@@ -0,0 +1,14 @@
+let double_name = "Raahim"; // ""
+let single_name = "Raahim"; // ''
+
+console.log(double_name, single_name);
+console.log(double_name.length);
+console.log(single_name[0]);
+
+// Template Literals
+
+let boy1 = "Raahim";
+let boy2 = "Furqan";
+let sentence = `${boy2} is a friend of ${boy1}`
+// Inside backticks you can use single quotes as well as double quotes.
+console.log(sentence)
