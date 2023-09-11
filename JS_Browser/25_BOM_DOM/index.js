e3bc04c1a57bea8f7f88d65b826913c43147d283
M.Raahim Rizwan <raahimrizwantcs@gmail.com>
2023-09-11 22:58:35 +0500

    BOM DOM

---
+++ JS_Browser/25_BOM_DOM/index.js
@@ -0,0 +1,3 @@
+console.log(window) // You can also write window.console.log(window)
+console.log(document)
+document.body.style.background = "lavender" // We can change css properties from javascript
