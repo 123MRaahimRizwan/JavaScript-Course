da5a5b99d7b2a9d82c2ee6d6b21bfc36a0d99e1f
M.Raahim Rizwan <raahimrizwantcs@gmail.com>
2023-09-24 19:07:43 +0500

    Closures

---
+++ JS_Browser/79_Closures/index.js
@@ -0,0 +1,50 @@
+// message = "Good Global";
+
+// function hello() {
+//   let message = "Good Morning";
+//   // let message = "Good Afternoon";
+//   console.log(message);
+//   let c = function hello2() {
+//     console.log("I am c" + message);
+//   };
+//   return c;
+// }
+
+// c = hello();
+// c();
+
+// // MDN Docs Example
+
+// function init() {
+//   var name = "Mozilla";
+//   function displayName() {
+//     console.log(name);
+//   }
+//   return displayName;
+// }
+
+// let d = init();
+// d();
+
+// Practice Func
+
+function returnFunc() {
+  const x = () => {
+    let a = 1;
+    console.log(a);
+    const y = () => {
+      let a = 2;
+      console.log(a);
+      const z = () => {
+        let a = 3;
+        console.log(a);
+      };
+      z();
+    };
+    y();
+  };
+  return x;
+}
+
+let e = returnFunc();
+e();
